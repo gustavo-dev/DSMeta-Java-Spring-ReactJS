0e8e038656805f08aa9c3e7f7391d629b1e4f103
Gustavo Henrique dos Santos <gustavohenrique.santos97@gmail.com>
2022-09-11 09:34:07 -0300

    Heroku -2

--- backend/src/main/java/com/springreact/dsmeta/services/SmsService.java
+++ backend/src/main/java/com/springreact/dsmeta/services/SmsService.java
@@ -1,51 +1,49 @@
 package com.springreact.dsmeta.services;
 
+import java.text.DecimalFormat;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.springreact.dsmeta.entities.Sale;
 import com.springreact.dsmeta.repositories.SaleRepository;
-import com.twilio.Twilio;
 import com.twilio.rest.api.v2010.account.Message;
 import com.twilio.type.PhoneNumber;
 
 @Service
 public class SmsService {
 
+	@Value("${twilio.sid}")
+	private String twilioSid;
 
-		@Value("${twilio.sid}")
-		private String twilioSid;
+	@Value("${twilio.key}")
+	private String twilioKey;
 
-		@Value("${twilio.key}")
-		private String twilioKey;
+	@Value("${twilio.phone.from}")
+	private String twilioPhoneFrom;
 
-		@Value("${twilio.phone.from}")
-		private String twilioPhoneFrom;
+	@Value("${twilio.phone.to}")
+	private String twilioPhoneTo;
 
-		@Value("${twilio.phone.to}")
-		private String twilioPhoneTo;
+	@Autowired
+	private SaleRepository saleRepository;
 
-		@Autowired
-		private SaleRepository saleRepository;
-		
-		public void sendSms(Long saleId) {
-			
-			Sale sale = saleRepository.findById(saleId).get();
+	public void sendSms(Long saleId) {
 
-			String date = sale.getDate().getMonthValue() + "/" + sale.getDate().getYear();
-					
-			String msg = "O vendedor " + sale.getSellerName() + " foi destaque em " + date
-					+ " com um total de R$" + String.format("%.2f", sale.getAmount());
-			
-			Twilio.init(twilioSid, twilioKey);
+		Sale sale = saleRepository.findById(saleId).get();
 
-			PhoneNumber to = new PhoneNumber(twilioPhoneTo);
-			PhoneNumber from = new PhoneNumber(twilioPhoneFrom);
+		String date = sale.getDate().getMonthValue() + "/" + sale.getDate().getYear();
 
-			Message message = Message.creator(to, from, msg).create();
+		String msg = "O vendedor " + sale.getSellerName() + " foi destaque em " + date + " com um total de R$ "
+				+ new DecimalFormat("#,##0.00").format(sale.getAmount());
 
-			System.out.println(message.getSid());
-		}
-	
+		PhoneNumber to = new PhoneNumber(twilioPhoneTo);
+		PhoneNumber from = new PhoneNumber(twilioPhoneFrom);
+
+		Message message = Message.creator(to, from, msg).create();
+
+		System.out.println(message.getSid());
+	}
+
 }
