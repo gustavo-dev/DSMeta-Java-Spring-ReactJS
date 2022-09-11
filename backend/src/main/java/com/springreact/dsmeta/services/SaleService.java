648a14f79318cc2fd1f24ecaa317f4add476cc9b
Gustavo Henrique dos Santos <gustavohenrique.santos97@gmail.com>
2022-09-10 22:03:30 -0300

    Date select

--- backend/src/main/java/com/springreact/dsmeta/services/SaleService.java
+++ backend/src/main/java/com/springreact/dsmeta/services/SaleService.java
@@ -1,10 +1,15 @@
 package com.springreact.dsmeta.services;
 
-import java.util.List;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import com.fasterxml.jackson.datatype.jsr310.deser.key.ZoneIdKeyDeserializer;
 import com.springreact.dsmeta.entities.Sale;
 import com.springreact.dsmeta.repositories.SaleRepository;
 
@@ -14,7 +19,16 @@ public class SaleService {
 	@Autowired
 	private SaleRepository repository;
 	
-	public List<Sale> findSale() {
-		return repository.findAll();
+	public Page<Sale> findSale(
+			String minDate, 
+			String maxDate,
+			Pageable pageable) {
+		
+		LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+		
+		LocalDate min = minDate.equals("")? today.minusDays(3650) : LocalDate.parse(minDate);
+		LocalDate max = maxDate.equals("")? today : LocalDate.parse(maxDate);
+		
+		return repository.findSales(min, max, pageable);
 	}
 }
